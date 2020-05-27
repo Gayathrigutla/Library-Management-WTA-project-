420f4ef174b8c4e8aef478ef9545edce92ea5c4f
Jayakrishnan <jayakrishnanb628@gmail.com>
2020-05-28 00:40:03 +0530

    Login & Sign Up done

--- server.js
+++ server.js
@@ -44,9 +44,19 @@ mysqlConnection.connect((err)=>{
     }
 });
 
+var msg = {
+    email : "",
+    password : ""
+};
+
+// GET requests
 
 app.get('/',function(req, res){
-    res.render('wtafrontpage.ejs');
+    res.render('login.ejs', {msg: msg});
+});
+
+app.get('/SignUp',function(req, res){
+    res.render('SignUp.ejs');
 });
 
 app.get('/cs.ejs',function(req, res){
@@ -64,6 +74,82 @@ app.get('/eee.ejs',function(req, res){
 app.get('/admin', function(req, res){
        
         res.render('admin.ejs');
+});
+
+//  POST requests
+
+app.post('/', urlencodedParser, function(req, res) {
+    console.log(req.body);
+    
+    mysqlConnection.query("SELECT * FROM users WHERE Email = ? AND password = ?",
+                            [req.body.Email, req.body.pass], function(err, result, fields) {
+        if (err) throw err;
+                
+        console.log(result);
+                
+        if(result.length > 0)
+        {
+            res.render('wtafrontpage.ejs');
+        }
+        else
+        {
+            mysqlConnection.query("SELECT * FROM users WHERE Email = ?",
+                                    [req.body.Email], function(err, result) {
+
+                console.log(result);
+
+                if (result.length == 0) {
+                    console.log('inside if');
+                    msg.email = "Invalid email ID.";
+                    res.render('login.ejs', {msg: msg});
+                } else {
+                    console.log(result.length);
+                    console.log('inisde else')
+                    msg.password = "Incorrect Password.";
+                    res.render('login.ejs', {msg: msg});
+                }
+
+            });
+        }
+
+    });
+        
+});
+
+// to be completed..
+
+app.post('/SignUp', urlencodedParser, function(req, res) {
+    console.log(req.body);
+    var user_details = {
+        username : req.body.name,
+        password : req.body.pass,
+        Email : req.body.Email,
+        Phone_number : req.body.phnumber
+        
+    };
+    console.log(user_details);
+
+    if(req.body.pass != req.body.cpass)
+    {
+        res.render('SignUp.ejs');
+    }
+    else
+    {  
+        mysqlConnection.query("INSERT INTO `new_sem4_project`.`users` SET ?",
+                                [user_details], function(err, result, fields) {
+            if(err)
+            {
+                throw err;
+            }
+            else
+            {
+                console.log(result);
+                res.render('wtafrontpage.ejs');
+            }
+        }); 
+             
+    }
+
 });
 
 app.post('/admin', urlencodedParser, upload.single('image'), function(req, res) {
@@ -88,15 +174,17 @@ app.post('/admin', urlencodedParser, upload.single('image'), function(req, res) 
                 subject_code: req.body.subject
             
             };
-            
-            mysqlConnection.query("SELECT * FROM `new_sem4_project`.`book` WHERE Book_name = ? AND  book_author = ?", [values.Book_name, values.book_author], function (err, result, fields) {
+             
+            mysqlConnection.query("SELECT * FROM `new_sem4_project`.`book` WHERE Book_name = ? AND  book_author = ?",
+                                    [values.Book_name, values.book_author], function (err, result, fields) {
                 if (err) throw err;
+                
                 console.log(result);
-                console.log(fields);
                 
                 if(result.length > 0)
                 {
-                    mysqlConnection.query("UPDATE `book` SET Availability = (Availability + 1) WHERE Book_name = ?", [values.Book_name], function (err, result, fields) {
+                    mysqlConnection.query("UPDATE `book` SET Availability = (Availability + 1) WHERE Book_name = ?", 
+                                            [values.Book_name], function (err, result, fields) {
                         if(err) throw err;
                         console.log(result);
                     })
